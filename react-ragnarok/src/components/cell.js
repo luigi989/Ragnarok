2f404cecd9e338d2e0b8be6fc2ad75af49dc38e9
Luigi989 <ludde.lindahl@gmail.com>
2022-05-02 23:41:31 +0200

    Improve tailwind css code

--- react-ragnarok/src/components/cell.js
+++ react-ragnarok/src/components/cell.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const Cell = ({ type, name, desc}) => {  
   return (
     <div className="group relative cursor-pointer transition ease-linear duration-300
-                    overflow-hidden w-fit">
-      <div className='absolute top-0 left-0 right-0 bottom-0 z-[-1] 
-                    border-[#9c5518] border-solid border-0 group-hover:border-4'/>
+                    overflow-hidden w-fit text-white">
+      <div className='absolute h-full w-full z-[20]
+                    border-[#9c5518] border-solid border-0 group-hover:border-4' />
       <img src={"/images/" + type + "/" + name + ".png"} alt={name} />
       <h1 className='absolute top-0 left-0 py-1 px-2 bg-[#9c5518]'>{name}</h1>
     </div>
