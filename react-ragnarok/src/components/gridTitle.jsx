9be7223d5d7b41f443191c0c7f380089a2c2d4ce
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 <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>
2022-05-02 17:01:18 +0200

    Add onClick function

--- react-ragnarok/src/components/gridTitle.jsx
+++ react-ragnarok/src/components/gridTitle.jsx
@@ -1,6 +1,6 @@
-const GridTitle = ({ name }) => {
+const GridTitle = ({ name, onClick }) => {
     return (
-        <div className="text-2xl text-yellow-600 ml-3">
+        <div onClick={onClick} className="text-2xl text-yellow-600 ml-3 hover:cursor-pointer w-fit">
           <h1 className="my-2 first-letter:uppercase">{name}</h1>
           {/* <img src={arrowRight} alt="arrowRight" /> */}
         </div>
