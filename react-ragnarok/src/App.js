9c3c04be0ed239e3b729435b6d7044ae55d99824
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 <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>
2022-05-02 14:30:49 +0200

    Reformat code

--- react-ragnarok/src/App.js
+++ react-ragnarok/src/App.js
@@ -1,64 +1,28 @@
 import React from 'react';
 import GridArea from './components/gridArea';
+import Header from './components/header';
+import Footer from './components/footer';
 // import Popup from './components/popup';
 
 import gods from './json/gods.json';
 import creatures from './json/creatures.json';
 import lore from './json/lore.json';
 
-const SubTitle = ({ name }) => {
-      return (
-        <div className="text-2xl text-yellow-600 ml-3">
-          <h1><a>{name}</a></h1>
-          {/* <img src={arrowRight} alt="arrowRight" /> */}
-        </div>
-      );
-  }
-  
-  const Header = () => {
-      return (
-        <header className='flex w-full'>  
-          <a href="Ragnarok.html">
-            <img src={'/images/pattern.png'} alt="Logo" />
-          </a>
-          {/* <form id="searchbar_form" className=''>
-            <input type="text" placeholder="Search..." name="search"></input>
-            <button type="submit">Submit</button> 
-          </form> */}
-        </header>
-      );
-  }
-  
-  const Footer = () => {
-      return (
-        <footer className='border-solid border-t-2 border-[#946c28] bg-[#412d0b] mt-6'>
-          <ul className='flex flex-col p-2 text-amber-200'>
-            <li>Contact Us</li>
-            <li>Site Info</li>
-            <li>References</li>
-          </ul>
-        </footer>
-      );
-  }
-
 function App() {
   return (
-            <React.StrictMode>
-            <Header />
-            <SubTitle name="Gods" />
-            <GridArea data={gods} type='gods'/>
-            <SubTitle name="Creatures" />
-            <GridArea data={creatures} type='creatures' />
-            <SubTitle name="Lore" />
-            <GridArea data={lore} type='lore' />
-            <Footer />
+    <React.StrictMode>
+      <Header />
+      <GridArea data={gods} type='gods' />
+      <GridArea data={creatures} type='creatures' />
+      <GridArea data={lore} type='lore' />
+      <Footer />
 
-            {/* {this.state.showPopup ?
+      {/* {this.state.showPopup ?
             <Popup name={this.state.name} description={this.state.description} 
                 closePopup={this.handleGodChange} /> 
             : null }   */}
 
-            </React.StrictMode>
+    </React.StrictMode>
 
   );
 }
